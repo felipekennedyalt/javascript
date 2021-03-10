df02ec76df4a9146346a52f30020f8114f1fb9ed
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-03-10 13:14:31 -0300

    1º desafio

---
+++ desafios/d001/script.js
@@ -0,0 +1,11 @@
+function primeiraMensagem() {
+    window.alert('Você clicou no 1º botão.')
+}
+
+function segundaMensagem() {
+    window.alert('Você clicou no 2º botão.')
+}
+
+function terceiraMensagem() {
+    window.alert('Você clicou no 3º botão.')
+}
