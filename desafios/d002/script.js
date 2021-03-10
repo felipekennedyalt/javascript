35697f1215c1fd2856fd67ac59cb37bdcf747b64
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-03-10 13:24:50 -0300

    2º desafio

---
+++ desafios/d002/script.js
@@ -0,0 +1,5 @@
+function bemVindo() {
+    let nome = window.prompt('Qual o seu nome?')
+    let idade = window.prompt('Qual sua idade?')
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos.`)
+}
