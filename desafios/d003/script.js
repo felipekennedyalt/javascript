7c09beac40e352ed363ea594f909ecd9c1f09437
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-03-11 14:39:30 -0300

    3º desafio

---
+++ desafios/d003/script.js
@@ -0,0 +1,6 @@
+function antSuc() {
+    let numero = window.prompt('Digite um número inteiro qualquer')
+
+    window.alert(`Antes de ${Number(numero)}, temos o número ${Number(numero) - 1}.
+Depois de ${Number(numero)}, temos o número ${Number(numero) + 1}`)
+}
